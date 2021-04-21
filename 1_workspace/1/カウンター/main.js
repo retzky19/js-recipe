7e63285707e867cd04a8978955f7c001868d7c5d
retzky19 <retzky39@gmail.com>
2021-04-21 13:30:00 +0900

    [□と○]を学習しました。

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -1,9 +1,19 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const kakeru2Button = document.getElementById("kakeru2-button")
 
 let count = 0
 
 plusButton.onclick = function() {
   count += 1
+  display.textContent = count
+}
+minusButton.onclick = function() {
+  count -= 1
+  display.textContent = count
+}
+kakeru2Button.onclick = function() {
+  count = count * 2
   display.textContent = count
 }
